98281346aeba9176873a9147c5e38fa65c210532
losrobbos <50549469+losrobbos@users.noreply.github.com>
2022-09-08 11:12:38 +0200

    Update Animal.d.ts

--- types/Animal.d.ts
+++ types/Animal.d.ts
@@ -1,10 +1,16 @@
 import { NextFunction, Request, Response } from "express";
 
+// Animal Input type
+// used to CREATE new animals (where we do not have an ID so far...)
 export type AnimalInput = {
   name: string;
   age?: number;
 };
 
+// Animal Type
+// type for an already created Animal in the database, has an ID
+// we use the Typescript Union operator (&) here to merge the Animal Input type with another type with an _id field
+// this way we get a new type with alle fields: { _id: string, name: string, age?: number }
 export type Animal = {
   _id: string;
 } & AnimalInput
