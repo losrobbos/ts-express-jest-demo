65d8f1ebe00f2f67410a597741276a0ea7846068
losrobbos <50549469+losrobbos@users.noreply.github.com>
2022-09-08 11:15:56 +0200

    Update AnimalController.ts

--- controllers/AnimalController.ts
+++ controllers/AnimalController.ts
@@ -12,11 +12,14 @@ export class AnimalController implements IController {
     this._animalService = animalService
   }
 
+  // use our animal service to get all animals from "database" and return response
   public getAll = (req: Request, res: Response, next: NextFunction) => {
     const animals = this._animalService.findAll();
     res.json(animals);
   };
 
+  // use animal service to store new animal in database 
+  // (just forward what the user sent to us in the body) 
   public store = (req: Request, res: Response) => {
     const animal = this._animalService.store(req.body);
     res.json(animal);
